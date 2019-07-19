b3b891109caa292ec3fafbc7f1acc4c45c5b0636
Lou Zuniga <louzuniga86@gmail.com>
2019-07-19 16:42:42 -0700

    more css mods

---
+++ script.js
@@ -0,0 +1,33 @@
+// /////////////////////////////////////////
+// jQuery Triggers*****************////////
+//////////////////////////////////////////
+
+$(function() {
+  //pinwheel loader
+  $(window).on('load', () => {
+    $('.loader .inner-loader').fadeOut(300, () => {
+      $('.loader').fadeOut(500);
+    });
+  });
+
+  // Navbar responsive function
+  $('#nav').on('click', event => {
+    let nav = document.getElementById('nav');
+    if (nav.className === 'nav-list') {
+      nav.className += ' responsive';
+    } else {
+      nav.className = 'nav-list';
+    }
+  });
+
+  // navbar responsive
+  $('.page-nav').click(function(event) {
+    const targetElement = $(this).attr('href');
+    const targetPosition = $(targetElement).offset().top;
+    $('html, body').animate({ scrollTop: targetPosition - 50 }, 'slow');
+  });
+
+  // Footer and copyright ************
+  let d = new Date();
+  $('#copyright').text(`Copyright \u00A9 ${d.getFullYear()}  Lou Zuniga`);
+});
