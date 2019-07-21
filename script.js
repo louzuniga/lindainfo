e4a4f1b0c7070ba0d2d623d2a36a79447c7f4f91
Lou Zuniga <louzuniga86@gmail.com>
2019-07-20 23:54:46 -0700

    updated youtube video

--- script.js
+++ script.js
@@ -51,6 +51,30 @@ modal.addEventListener('click', function() {
   this.style.display = 'none';
 });
 
+// Google sheets for email about stories
+const TO_ADDRESS = 'louzuniga86@gmail.com';
+
+function doPost(e) {
+  try {
+    Logger.log(e); // the Google Script version of console.log see: Class Logger
+    MailApp.sendEmail(
+      TO_ADDRESS,
+      'Contact Form Submitted',
+      JSON.stringify(e.parameters)
+    );
+    // return json success results
+    return ContentService.createTextOutput(
+      JSON.stringify({ result: 'success', data: JSON.stringify(e.parameters) })
+    ).setMimeType(ContentService.MimeType.JSON);
+  } catch (error) {
+    // if error return this
+    Logger.log(error);
+    return ContentService.createTextOutput(
+      JSON.stringify({ result: 'error', error: e })
+    ).setMimeType(ContentService.MimeType.JSON);
+  }
+}
+
 // /////////////////////////////////////////
 // jQuery Triggers*****************////////
 //////////////////////////////////////////
